2e0c2717aee97b7b78b8bc090a7ca93a19837c7e
jasonw <jasonw@eyebuydirect.com>
2021-12-08 18:14:29 +0800

    remove useless code

--- app/Exceptions/InvalidRequestException.php
+++ app/Exceptions/InvalidRequestException.php
@@ -2,20 +2,10 @@
 
 namespace App\Exceptions;
 
-use App\Http\Controllers\Controller;
 use Exception;
 
 class InvalidRequestException extends Exception
 {
-    public function __construct(string $message)
-    {
-        parent::__construct($message);
-    }
 
-    public function render()
-    {
-        $controller = new Controller();
-        return $controller->fail($this->message);
-    }
 }
 
--- app/Exceptions/ResponseSystemException.php
+++ app/Exceptions/ResponseSystemException.php
@@ -2,20 +2,10 @@
 
 namespace App\Exceptions;
 
-use App\Http\Controllers\Controller;
 use Exception;
 
 class ResponseSystemException extends Exception
 {
-    public function __construct(string $code)
-    {
-        parent::__construct($code);
-    }
 
-    public function render()
-    {
-        $controller = new Controller();
-        return $controller->failSystem($this->code);
-    }
 }
 
